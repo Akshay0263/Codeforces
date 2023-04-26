53451ff17520af8846402e5b8451c34a3479b93a
Akshay Raj <akshay.akshat0263@gmail.com>
2023-04-26 23:12:41 +0530

    q2 corrected

--- CF849/Q2.py
+++ CF849/Q2.py
@@ -6,21 +6,12 @@ for i in range(n):
     word = input()
     for j in range(l):
         if word[j]=='U': coord[1]+=1
-        if coord == [1,1]:
-            print("YES")
-            break
-        if word[j]=='D': coord[1]-=1
-        if coord == [1,1]:
-            print("YES")
-            break
-        if word[j]=='L': coord[0]+=1
-        if coord == [1,1]:
-            print("YES")
-            break
-        if word[j]=='R': coord[0]+=1
-        if coord == [1,1]:
-            print("YES")
-            break
+        elif word[j]=='D': coord[1]-=1
+        elif word[j]=='L': coord[0]-=1
+        elif word[j]=='R': coord[0]+=1
+        if coord == [1,1]: print("YES"); break
+
+        
 #        print(word[j],"+", coord)
 
     if coord != [1,1]: print("NO")
